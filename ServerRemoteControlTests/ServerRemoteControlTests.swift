3a9dc4137a7fcabc19ac2d6f96b09fab933f5165
Victor Yurkin <viy2003@pc171816.med.cornell.edu>
2016-03-18 14:45:41 -0400

    Initial Commit

---
+++ ServerRemoteControlTests/ServerRemoteControlTests.swift
@@ -0,0 +1,36 @@
+//
+//  ServerRemoteControlTests.swift
+//  ServerRemoteControlTests
+//
+//  Created by Victor Yurkin on 3/18/16.
+//  Copyright © 2016 WCMC. All rights reserved.
+//
+
+import XCTest
+@testable import ServerRemoteControl
+
+class ServerRemoteControlTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
