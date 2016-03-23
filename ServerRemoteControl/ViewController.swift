2135af7bbe3467316d61da1f1fb359ee990e73ec
Victor Yurkin <viy2003@pc171816.med.cornell.edu>
2016-03-23 14:54:46 -0400

    Dist. v1

--- ServerRemoteControl/ViewController.swift
+++ ServerRemoteControl/ViewController.swift
@@ -12,9 +12,44 @@ import NMSSH
 
 class ViewController: UIViewController {
 
+    var sftp:NMSFTP!
+    var timer: dispatch_source_t!
+    
+    @IBOutlet weak var labelStatus: UILabel!
+    @IBOutlet weak var labelResponse: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let session = NMSSHSession.connectToHost("140.251.7.146", port: 22, withUsername: "viy2003")
+        if(session.connected) {
+            session.authenticateByPassword("86V89y12g")
+            if(!session.authorized) {
+                print("Failed")
+            }
+        }
+        let sftp = NMSFTP.connectWithSession(session)
+        if(!sftp.connected) {
+            print("SFTP Failed")
+        }else {
+            self.sftp = sftp
+        }
+        
+        // Start monitoring status
+        self.startTimer()
+        
+        //Check apache status
+        //do {
+            //let response = try sftp.session.channel.execute("/bin/bash /var/www/html/server_control_api/status.sh")
+            //let response = try self.sftp!.session.channel.execute("php /var/www/html/server_control_api/index.php")
+            //self.labelStatus.text = "Current Status: " + response
+            //if(response == "down") {
+            //    self.labelStatus.textColor = UIColor.redColor()
+            //}
+        //} catch {
+        //    print("An error occurred.")
+        //}
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,24 +70,36 @@ class ViewController: UIViewController {
         let filePath = NSBundle.mainBundle().pathForResource("Test", ofType: "txt")
         session.channel.uploadFile(filePath, to: "/var/www/html/test.txt")
         */
-
-        let session = NMSSHSession.connectToHost("140.251.7.146", port: 22, withUsername: "viy2003")
-        if(session.connected) {
-            session.authenticateByPassword("86V89y12g")
-            if(session.authorized) {
-                print("Success")
-            }
-        }
-        let sftp = NMSFTP.connectWithSession(session)
-        if(sftp.connected) {
-            print("SFTP Success")
-        }
         
-        let filePath = NSBundle.mainBundle().pathForResource("Test", ofType: "txt")
-        sftp.writeFileAtPath(filePath, toFileAtPath: "/var/www/html/test/test123.txt")
+        let filePath = NSBundle.mainBundle().pathForResource("restart", ofType: "txt")
+        self.sftp!.writeFileAtPath(filePath, toFileAtPath: "/var/www/html/server_control_api/restart.txt")
         
-        
+        self.labelResponse.text = "restart requested..."
         
     }
+    
+    func startTimer() {
+        let queue = dispatch_queue_create("com.domain.app.timer", nil)
+        timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue)
+        dispatch_source_set_timer(timer, DISPATCH_TIME_NOW, 10 * NSEC_PER_SEC, 1 * NSEC_PER_SEC) // every 60 seconds, with leeway of 1 second
+        dispatch_source_set_event_handler(timer) {
+            self.updateStatus();
+        }
+        dispatch_resume(timer)
+    }
+    
+    func stopTimer() {
+        dispatch_source_cancel(timer)
+        timer = nil
+    }
+    
+    func updateStatus() {
+        dispatch_async(dispatch_get_main_queue(), {
+            let response = self.sftp.contentsAtPath("/var/www/html/server_control_api/response.txt")
+            let responseTxt = NSString(data: response, encoding: NSUTF8StringEncoding) as! String
+            self.labelResponse.text = responseTxt
+        })
+    }
+    
 }
 
